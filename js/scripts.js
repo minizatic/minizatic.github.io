fa99550b421c55dc211a6561095ecd92ad011680
Blake Patches <blakepatches@gmail.com>
2024-01-04 21:07:29 -0500

    Add new landing page, make copyright year auto-update

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,3 @@
-let socket = new WebSocket("wss://radio.bpatches.com/api/live/nowplaying/websocket");
-
-socket.onopen = function(e) {
-  socket.send(JSON.stringify({
-    "subs": {
-      "station:radio": {}
-    }
-  }));
-};
-
-socket.onmessage = function(event) {
-  const data = JSON.parse(event.data);
-  const np = data?.pub?.data?.np || null;
-  if (np) {
-    document.getElementById("current-listeners").textContent = np.listeners.current;
-  }
-};
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("copyright-year").textContent = new Date().getFullYear();
+});
